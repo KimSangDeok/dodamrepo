ad7944c39ab7ea3022cdb8f16a15c662a5dbb986
kosta <kosta@kosta-PC>
2017-06-20 09:56:35 +0900

    선미 - 고객관리

---
+++ Dodam/src/myproj/customer/controller/customerController.java
@@ -0,0 +1,24 @@
+package myproj.customer.controller;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+@RequestMapping("/customer")
+public class customerController {
+
+	@RequestMapping("/{url}.dodam")
+	public String showList(@PathVariable String url){
+		return "/customer/"+url;
+	}
+	
+	
+}
